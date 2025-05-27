9412c3b25da23dd89e25a3c8032f671dd2fcd3bc
Kit221 <golov.leonid.2018@gmail.com>
2025-05-27 20:20:29 +0300

    Диплом. React

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-// import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+/**
+ * Точка входа в приложение.
+ * Рендерит корневой компонент App в DOM.
+ */
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
